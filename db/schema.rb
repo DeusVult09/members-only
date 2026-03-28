ffc1796e54ef081a3063102b20190f944689ac70
Zarifa <mamadalievazara@gmail.com>
2026-03-28 16:57:23 +0100

    Create Post model, run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_26_230710) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_154953) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -33,5 +42,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_26_230710) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "tasks", "users"
 end
