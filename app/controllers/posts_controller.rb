28b739a8dd026551b1f51fa090fa717ce7fd1fa8
Zarifa <mamadalievazara@gmail.com>
2026-03-28 23:30:06 +0100

    Build create action, add #index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,22 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = current_user.posts.build(post_params)
+
+    if @post.save?
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private 
+
+  def post_params
+    params.expect(post: [ :title, :body ] )
   end
 end
