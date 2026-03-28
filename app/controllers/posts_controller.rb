93462f532dcbb475c7a5585975b3005bb248da9f
Zarifa <mamadalievazara@gmail.com>
2026-03-29 00:17:54 +0100

    Fix post form: add labels and remove url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,10 +17,11 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+
+    
   end
 
   private 
-
   def post_params
     params.expect(post: [ :title, :body ] )
   end
