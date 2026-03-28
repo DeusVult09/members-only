63fdb03d9f0c81ab1e1dcd468b4ccb39504d9efc
Zarifa <mamadalievazara@gmail.com>
2026-03-28 22:30:42 +0100

    Define resources for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :posts, only: [:new, :create, :index]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
