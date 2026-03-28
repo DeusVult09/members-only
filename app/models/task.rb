da743881af52c17770c3e6ab40fd2a02bd16d080
Zarifa <mamadalievazara@gmail.com>
2026-03-28 22:44:43 +0100

    Write a form in new.html.erb to create a post

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
 
   enum :status, { draft: 0, underway: 1, done: 2, archived: 3 }
+
+  validates :title, presence: true 
+  validates :completed, inclusion: { in: [true, false] }
 end
